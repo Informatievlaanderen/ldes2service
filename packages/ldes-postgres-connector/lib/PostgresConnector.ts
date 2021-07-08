988fc49e5628b37e508a817048a3b1b962094944
Carlos Ruiz <d34dplayer@protonmail.com>
2021-07-08 13:36:23 +0200

    Added an amountOfVersions to the connector

--- packages/ldes-postgres-connector/lib/PostgresConnector.ts
+++ packages/ldes-postgres-connector/lib/PostgresConnector.ts
@@ -9,8 +9,9 @@ const POSTGRES_PORT = Number.parseInt(process.env.POSTGRES_PORT || '5432', 10);
 
 export class PostgresConnector implements IWritableConnector {
   private readonly pool: Pool;
+  private readonly configuration: Record<string, any>;
 
-  public constructor() {
+  public constructor(configuration: Record<string, any>) {
     this.pool = new Pool({
       user: POSTGRES_USER,
       host: POSTGRES_HOST,
@@ -18,6 +19,8 @@ export class PostgresConnector implements IWritableConnector {
       password: POSTGRES_PASSWORD,
       port: POSTGRES_PORT,
     });
+
+    this.configuration = configuration;
   }
 
   /**
@@ -26,6 +29,19 @@ export class PostgresConnector implements IWritableConnector {
    */
   public async writeVersion(member: string): Promise<void> {
     const memberObject = JSON.parse(member);
+    if (this.configuration.amountOfVersions) {
+      await this.writeLatestVersions(memberObject, this.configuration.amountOfVersions);
+    } else {
+      await this.writeEveryVersion(memberObject);
+    }
+  }
+
+  /**
+   * Writes a version to the corresponding backend system, if we store all the versions.
+   * @param memberObject
+   * @private
+   */
+  private async writeEveryVersion(memberObject: Record<string, any>): Promise<void> {
     // LDES client can send us the same member several times, so we only add the ones with a new unique @id.
     const query = 'INSERT INTO "ldes" VALUES ($1, $2, $3, $4, $5) ON CONFLICT ("@id") DO NOTHING;';
 
@@ -46,6 +62,58 @@ export class PostgresConnector implements IWritableConnector {
         console.log('New entry added to the postgres database.');
       }
     });
+  }
+
+  /**
+   * Writes a version to the corresponding backend system, if we only want to keep the X latest versions.
+   * @param memberObject
+   * @param amountOfVersions
+   * @private
+   */
+  private async writeLatestVersions(
+    memberObject: Record<string, any>,
+    amountOfVersions: number,
+  ): Promise<void> {
+    try {
+      // 1st request: Check that the new member isn't already in the db.
+      const query1 = 'SELECT count(*) as c FROM ldes WHERE "@id"=$1;';
+      const res = await this.pool.query(query1, [memberObject['@id']]);
+      if (Number.parseInt(res.rows[0].c, 10)) {
+        console.log('Member already exists.', memberObject['@id']);
+        return;
+      }
+
+      // 2nd request: Amount of versions currently in the db.
+      const query2 = 'SELECT count("@id") as c FROM ldes WHERE "isVersionOf"=$1;';
+      const versions = await this.pool.query(query2, [
+        memberObject['http://purl.org/dc/terms/isVersionOf']['@id'],
+      ]);
+      if (versions.rowCount && Number.parseInt(versions.rows[0].c, 10) < amountOfVersions) {
+        console.log('Version limit not reached.', versions.rows[0].c, amountOfVersions);
+        return this.writeEveryVersion(memberObject);
+      }
+
+      // If == amountOfVersions, we need to get rid of one.
+      // 3rd request: Get the timestamp of the oldest version in the DB.
+      const query3 = `SELECT "@id", "generatedAtTime" FROM ldes 
+          WHERE "isVersionOf"=$1 ORDER BY "generatedAtTime" LIMIT 1;`;
+      const oldestVersion = await this.pool.query(query3, [
+        memberObject['http://purl.org/dc/terms/isVersionOf']['@id'],
+      ]);
+      const dbOldestDate = PostgresConnector.getDate(oldestVersion.rows[0].generatedAtTime);
+      const memberDate = PostgresConnector.getDate(memberObject['http://www.w3.org/ns/prov#generatedAtTime']);
+      if (memberDate && dbOldestDate && memberDate > dbOldestDate) {
+        // We get rid of the oldest one, either the new one or the one in the DB. (0 or 2 requests)
+        const query4 = 'DELETE FROM ldes WHERE "@id"=$1';
+        await this.pool.query(query4, [oldestVersion.rows[0]['@id']]);
+        // Add the new member
+        console.log('Member replaced older version.');
+        return this.writeEveryVersion(memberObject);
+      }
+      console.log('Member is older than what we currently store.', dbOldestDate, memberDate);
+    } catch (error: unknown) {
+      console.error(error);
+    }
   }
 
   /**
