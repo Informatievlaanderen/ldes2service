fad5ba9279567bbb08d10cd0ffe4a1c57cbffebd
Carlos Ruiz <d34dplayer@protonmail.com>
2021-07-14 16:04:10 +0200

    Use Redis state in the replicator

--- packages/ldes-replicator/bin/ldes-replicator.ts
+++ packages/ldes-replicator/bin/ldes-replicator.ts
@@ -2,8 +2,8 @@
  * CLI interface where manual dependency injection happens
  */
 
-import { DummyState } from '@ldes/ldes-dummy-state';
 import { PostgresConnector } from '@ldes/ldes-postgres-connector';
+import { RedisState } from '@ldes/ldes-redis-state';
 
 import { newEngine } from '@treecg/actor-init-ldes-client';
 import { Orchestrator } from '../lib/Orchestrator';
@@ -15,7 +15,9 @@ const POLL_INTERVAL = Number.parseInt(process.env.pollingInterval ?? '5000', 10)
 
 async function run(): Promise<void> {
   const connector = new PostgresConnector({ amountOfVersions: 0, databaseName: 'ldes' });
-  const state = new DummyState();
+  const state = new RedisState({
+    id: 'replicator',
+  });
 
   const options = {
     pollingInterval: POLL_INTERVAL,
