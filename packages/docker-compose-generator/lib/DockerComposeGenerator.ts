aa341fa6c8d836ce297deaf04c2ac74ef7c7d5dd
Carlos Ruiz <d34dplayer@protonmail.com>
2021-07-15 16:33:56 +0200

    Add a replicator service  to the docker compose generator

--- packages/docker-compose-generator/lib/DockerComposeGenerator.ts
+++ packages/docker-compose-generator/lib/DockerComposeGenerator.ts
@@ -1,5 +1,5 @@
 import type { IGeneratorApiSetup } from '@ldes/types';
-import { parse } from 'yaml';
+import { parse, stringify } from 'yaml';
 
 const composer = require('docker-composer');
 const format = require('string-format');
@@ -16,20 +16,77 @@ export class DockerComposeGenerator {
     this.options = options;
   }
 
-  public generate(services: string[], settings: Record<string, any>): string {
-    const dockerFile: IComposeFile = {
+  public generate(services: Record<string, any>, replicator: Record<string, any>): string {
+    let dockerFile: IComposeFile = {
       version: '3.9',
       services: {},
     };
 
     for (const service of this.options) {
-      if (services.includes(service.id)) {
-        const formattedService = parse(format(service.composeTemplate, settings[service.id]));
+      if (Object.keys(services).includes(service.id)) {
+        const formattedService = parse(format(service.composeTemplate, services[service.id].settings));
 
         Object.assign(dockerFile.services, formattedService);
       }
     }
+    dockerFile = DockerComposeGenerator.replicatorSetup(dockerFile, services, replicator);
+    composer.generate(dockerFile);
 
-    return composer.generate(dockerFile);
+    return stringify(dockerFile);
+  }
+
+  /**
+   * Once all the services have been added, this method sets up a replicator service configured
+   * to connect to said services.
+   * @param dockerFile the compose file 'object"
+   * @param services the services added and their settings
+   * @param replicator the replicator settings
+   * @private
+   */
+  private static replicatorSetup(
+    dockerFile: IComposeFile,
+    services: Record<string, any>,
+    replicator: Record<string, any>
+  ): IComposeFile {
+    if (replicator.redis) {
+      replicator.state = {
+        id: 'replicator',
+        host: 'redis',
+        port: 6_379,
+      };
+
+      Object.assign(dockerFile.services, {
+        redis: {
+          image: 'redis:6',
+          command: 'redis-server --appendonly yes',
+          ports: ['6379:6379'],
+        },
+      });
+    }
+
+    const ldesReplicator: Record<string, any> = {
+      replicator: {
+        image: 'ghcr.io/osoc21/ldes-replicator',
+        environment: {
+          URLS: replicator.urls,
+          STATE_CONFIG: JSON.stringify(replicator.state),
+          POLL_INTERVAL: Number.parseInt(replicator.polling_interval || '5000', 10),
+        },
+      },
+    };
+
+    const CONNECTORS = Object.keys(services);
+    ldesReplicator.replicator.environment.CONNECTORS = JSON.stringify(CONNECTORS);
+
+    for (const con of CONNECTORS) {
+      ldesReplicator.replicator.environment[`CONNECTOR_${con}_TYPE`] = services[con].type;
+      ldesReplicator.replicator.environment[`CONNECTOR_${con}_CONFIG`] = JSON.stringify(
+        services[con].settings
+      );
+    }
+
+    Object.assign(dockerFile.services, ldesReplicator);
+
+    return dockerFile;
   }
 }
