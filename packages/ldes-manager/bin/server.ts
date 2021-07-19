f22339fbbf494c40cab1f9bd9ea8fd94c7abc725
Carlos Ruiz <d34dplayer@protonmail.com>
2021-07-19 15:01:01 +0200

    Refactor server.ts

--- packages/ldes-manager/bin/server.ts
+++ packages/ldes-manager/bin/server.ts
@@ -1,22 +1,29 @@
-import Fastify, { FastifyInstance } from 'fastify';
+import type { FastifyInstance } from 'fastify';
+import Fastify from 'fastify';
 import AvailableConnectorsRoute from '../lib/routes/AvailableConnectors.route';
 import ConnectorRoute from '../lib/routes/Connector.route';
+import { router as GeneratorRoute, generatorSetup } from '../lib/routes/Generator.route';
 import OrchestratorRoute from '../lib/routes/Orchestrator.route';
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 7_000;
 const server: FastifyInstance = Fastify({});
 
-server.register(ConnectorRoute);
-server.register(OrchestratorRoute);
-server.register(AvailableConnectorsRoute);
-
-const start = async () => {
+const start = async (): Promise<void> => {
   try {
+    await Promise.all([
+      server.register(ConnectorRoute),
+      server.register(OrchestratorRoute),
+      server.register(AvailableConnectorsRoute),
+      server.register(GeneratorRoute),
+    ]);
     await server.listen(port);
+    await generatorSetup();
     console.log('Server started successfully');
-  } catch (err) {
-    server.log.error(err);
+  } catch (error: unknown) {
+    console.error(error);
+    server.log.error(error);
     process.exit(1);
   }
 };
+
 start();
