45134e4a44e1836f430c2807f220ee7a01624b84
Carlos Ruiz <d34dplayer@protonmail.com>
2021-07-19 16:38:37 +0200

    Use the generator plugin isntead of being a proxy

--- packages/ldes-manager/bin/server.ts
+++ packages/ldes-manager/bin/server.ts
@@ -1,8 +1,9 @@
+import type { IGeneratorPluginOptions } from '@ldes/types';
 import type { FastifyInstance } from 'fastify';
 import Fastify from 'fastify';
 import AvailableConnectorsRoute from '../lib/routes/AvailableConnectors.route';
 import ConnectorRoute from '../lib/routes/Connector.route';
-import { router as GeneratorRoute, generatorSetup } from '../lib/routes/Generator.route';
+import GeneratorRoute from '../lib/routes/Generator.route';
 import OrchestratorRoute from '../lib/routes/Orchestrator.route';
 
 const port = process.env.PORT || 7_000;
@@ -14,10 +15,9 @@ const start = async (): Promise<void> => {
       server.register(ConnectorRoute),
       server.register(OrchestratorRoute),
       server.register(AvailableConnectorsRoute),
-      server.register(GeneratorRoute),
+      server.register<IGeneratorPluginOptions>(GeneratorRoute, { setup: false, prefix: '/generator' }),
     ]);
     await server.listen(port);
-    await generatorSetup();
     console.log('Server started successfully');
   } catch (error: unknown) {
     console.error(error);
