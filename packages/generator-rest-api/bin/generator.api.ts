f904bca1e3ba8f69fe07d92a4d03e855b5e6fec8
Carlos Ruiz <d34dplayer@protonmail.com>
2021-07-20 16:50:40 +0200

    Add replicator to the helmfile generator

--- packages/generator-rest-api/bin/generator.api.ts
+++ packages/generator-rest-api/bin/generator.api.ts
@@ -72,7 +72,7 @@ fastify.post(
   async (_request: any, _reply: any) => {
     switch (_request.body.type) {
       case 'helm':
-        _reply.send(helmFileGenerator.generate(_request.body.services));
+        _reply.send(helmFileGenerator.generate(_request.body.services, _request.body.replicator));
         break;
       case 'compose':
         _reply.send(dockerComposeGenerator.generate(_request.body.services, _request.body.replicator));
